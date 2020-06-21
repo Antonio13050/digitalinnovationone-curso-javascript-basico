ab8ff8c452cf211190e343e7fa1f4ef5e3ed0200
Antonio <antonio13050@gmail.com>
2020-06-21 15:17:00 -0300

    introdução ao Javascript

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var nome = "Antonio";
+var n1 = 27;
+var n2 = "10";
+var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos.");
+//alert(idade + idade2);
+console.log(n1 * n2);
+console.log(frase);
+console.log(frase.replace("Japão", "Brasil"));
+console.log(frase.toUpperCase());
